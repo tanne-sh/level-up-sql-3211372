4cdb9072ca56dfd28027b51f492a044d25a13e86
Tanya Shurinova <61487027+tanne-sh@users.noreply.github.com>
2025-01-08 01:49:54 +0000

    New customer addedd to the table

--- notes.sql
+++ notes.sql
@@ -34,3 +34,9 @@ FROM Dishes
 WHERE Type != 'Beverage'
 ORDER BY Type;
 
+INSERT INTO Customers ( FirstName, LastName, Email, 
+Address, City, State, Phone, Birthday)
+VALUES ( 'Anna', 'Smith', 'asmith@samoca.org','479 Lapis Dr.', 
+'Memphis', 'TN', '555) 555-1212', '1973-07-21');
+
+SELECT * FROM Customers ORDER BY CustomerID DESC;
