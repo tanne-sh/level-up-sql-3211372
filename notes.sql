2963130f3ba0bd9e9d96790b92c68ff7b4f082ed
Tanya Shurinova <61487027+tanne-sh@users.noreply.github.com>
2025-01-12 16:18:58 +0000

    Update info about FavoriteDish for Customer

--- notes.sql
+++ notes.sql
@@ -140,3 +140,22 @@ JOIN OrdersDishes ON Dishes.DishID = OrdersDishes.DishID
 WHERE OrdersDishes.OrderID = 1001;
 
 SELECT DishID
+FROM Dishes
+WHERE Name ='Quinoa Salmon Salad';
+
+SELECT *
+FROM Customers
+WHERE FirstName = 'Cleo'
+  AND LastName = 'Goldwater';
+
+UPDATE Customers
+SET FavoriteDish = 9
+WHERE CustomerID = 42;
+
+UPDATE Customers
+SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad')
+WHERE CustomerID = 42;
+
+SELECT Customers.FirstName, Customers.LastName, Customers.FavoriteDish, Dishes.Name
+FROM Customers
+JOIN Dishes ON Customers.FavoriteDish = Dishes.DishID
