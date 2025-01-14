6f841a2debf93b76ae49bfd5ef35ac6278545515
Tanya Shurinova <61487027+tanne-sh@users.noreply.github.com>
2025-01-14 03:57:42 +0000

    Add new books to the library database

--- notes_library.sql
+++ notes_library.sql
@@ -22,3 +22,13 @@ SELECT
     WHERE Books.Title = 'Dracula'
     AND Loans.ReturnedDate IS NULL)
 AS AvailableCopies;
+
+INSERT INTO Books(Title, Author, Published, Barcode)
+VALUES ('Dracula', 'Bram Stoker', '1897', '4819277482'),
+('Gulliver’s Travels into Several Remote Nations of the World', 
+'Jonathan Swift', '1729', '4899254401');
+
+SELECT *
+FROM Books
+ORDER BY BookID DESC
+LIMIT 5;
