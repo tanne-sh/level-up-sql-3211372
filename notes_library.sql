f1653f5a0f1dcf893124a50eb7a01eac0eae0ac1
Tanya Shurinova <61487027+tanne-sh@users.noreply.github.com>
2025-01-15 02:19:16 +0000

    Check out book's informsation with patrionID

--- notes_library.sql
+++ notes_library.sql
@@ -32,3 +32,35 @@ SELECT *
 FROM Books
 ORDER BY BookID DESC
 LIMIT 5;
+
+INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
+VALUES (
+  (SELECT BookID
+   FROM Books
+   WHERE Barcode = '2855934983'),
+   (SELECT PatronID
+   FROM Patrons
+   WHERE Email = 'jvaan@wisdompets.com'),
+   2022-08-25,
+   2022-09-08 
+    );
+
+    INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
+VALUES (
+  (SELECT BookID
+   FROM Books
+   WHERE Barcode = '4043822646'),
+   (SELECT PatronID
+   FROM Patrons
+   WHERE Email = 'jvaan@wisdompets.com'),
+   2022-08-25,
+   2022-09-08 
+    );
+
+SELECT *
+FROM Loans
+JOIN Books ON Loans.BookID = Books.BookID
+WHERE PatronID = (
+  SELECT PatronID
+  FROM Patrons
+  WHERE Email = 'jvaan@wisdompets.com');
